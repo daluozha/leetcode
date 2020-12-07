76f6ac6ec3fa679934a024eda0130812f9336815
da1uozha <huangyucheng@smart3f.com>
2020-12-07 19:40:26 +0800

    206

---
+++ 206. Reverse Linked List.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+
+struct ListNode* reverseList(struct ListNode* head){
+    // 迭代
+    struct ListNode* p=NULL;
+    struct ListNode* q=head;
+    while(q!=NULL){
+        struct ListNode* tmp=q->next;
+        q->next=p;
+        p=q;
+        q=tmp;
+    }
+    return p;
+    
+}
