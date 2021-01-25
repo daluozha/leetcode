4759b89fb1f4dd450b6e82efe567491958ec13f4
da1uozha <huangyucheng@smart3f.com>
2021-01-25 09:49:17 +0800

    189

--- 189. Rotate Array.js
+++ 189. Rotate Array.js
@@ -4,6 +4,15 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var rotate = function(nums, k) {
+    while(k!=0){
+        // 法一
+        // let last=nums.splice(nums.length-1,1)
+        // nums.unshift(last)
+        // 法二
+        nums.unshift(nums.pop())
+        k--
+    }
+    return nums
     // 另一种解法：前k个和后k个分别倒置，然后整体倒置
-    while(k > 0) (nums.unshift(nums.pop()), k--)
+    // while(k > 0) (nums.unshift(nums.pop()), k--)
 };
