c24b27fa420f307e2f2db5fbdefb7a31797961f0
daluozha <561890199@qq.com>
2021-02-08 09:24:23 +0800

    509

--- 509. Fibonacci Number.js
+++ 509. Fibonacci Number.js
@@ -2,7 +2,16 @@
  * @param {number} n
  * @return {number}
  */
-var map = {}
 var fib = function(n) {
-    return n < 2 ? n : map[n] ? map[n] : map[n] = fib(n-1) + fib(n-2)
+    // 法一
+    // let map = {}
+    // return n < 2 ? n : map[n] ? map[n] : map[n] = fib(n-1) + fib(n-2)
+
+    // 法二
+    let dp=new Array(n+1).fill(0)
+    dp[1]=1
+    for(let i=2;i<n+1;i++){
+        dp[i]=dp[i-1]+dp[i-2]
+    }
+    return dp[n]
 };
