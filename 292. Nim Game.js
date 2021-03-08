81bb053f9206256e6f0f373fc9ef3db187248ae7
daluozha <561890199@qq.com>
2021-03-08 19:41:18 +0800

    292

---
+++ 292. Nim Game.js
@@ -0,0 +1,28 @@
+/**
+ * tag:动态规划
+ * @param {number} n
+ * @return {boolean}
+ */
+var canWinNim = function(n) {
+    // 法一：
+    // return n % 4 
+
+    // 法二：
+    // if(n < 4) return true
+    // let dp = new Array(n+1).fill(false)
+    // dp[1] = dp[2] = dp[3] = true
+    // for(let i = 4;i <= n;i++) 
+    //     dp[i] = !(dp[i - 1] && dp[i - 2] && dp[i - 3])
+    // return dp[n]
+
+    // 法二优化版
+    if(n < 4) return true
+    let one = true,two = true,three = true,cur
+    for(let i = 4;i <= n;i++){
+        cur = !(one && two && three)
+        one = two
+        two = three
+        three = cur
+    }
+    return cur
+};
