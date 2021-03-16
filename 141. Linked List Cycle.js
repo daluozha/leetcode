83231105225c8403f301112649a9ff7904510dea
daluozha <561890199@qq.com>
2021-03-16 22:44:28 +0800

    141

--- 141. Linked List Cycle.js
+++ 141. Linked List Cycle.js
@@ -11,7 +11,7 @@
  * @return {boolean}
  */
 var hasCycle = function(head) {
-    // 法一
+    // 法一 JSON.stringify当在循环引用时会抛出异常TypeError ("cyclic object value")（循环对象值）
     // try{
     //     JSON.stringify(head)
     //     return false
