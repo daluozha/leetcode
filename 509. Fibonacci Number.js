c395d38dffb9df24347d2f529ce8563378fd807f
daluozha <561890199@qq.com>
2021-03-20 14:00:41 +0800

    509

--- 509. Fibonacci Number.js
+++ 509. Fibonacci Number.js
@@ -1,17 +1,34 @@
 /**
+ * tag:动态规划
  * @param {number} n
  * @return {number}
  */
+
+// 法一
+
+var fib = function(n,map = {}) {
+    return n < 2 ? n : map[n] ? map[n] : map[n] = fib(n-1,map) + fib(n-2,map)
+};
+
 var fib = function(n) {
-    // 法一
-    // let map = {}
-    // return n < 2 ? n : map[n] ? map[n] : map[n] = fib(n-1) + fib(n-2)
 
     // 法二 动态规划
-    let dp=new Array(n+1).fill(0)
-    dp[1]=1
-    for(let i=2;i<n+1;i++){
-        dp[i]=dp[i-1]+dp[i-2]
+    // let dp = new Array(n + 1).fill(0)
+    // dp[1] = 1
+    // for(let i = 2;i <= n;i++){
+    //     dp[i] = dp[i - 1] + dp[i - 2]
+    // }
+    // return dp[n]
+
+    // 法三 dp优化
+    if(n < 2) return n
+    let one = 0,
+        two = 1,
+        cur
+    for(let i = 2;i <= n;i++){
+        cur = one + two
+        one = two
+        two = cur
     }
-    return dp[n]
+    return cur
 };
