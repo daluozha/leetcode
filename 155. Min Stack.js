c6bb9929257d25284c84d14a1e3a3fa4358e3e6e
daluozha <561890199@qq.com>
2021-03-24 20:57:08 +0800

    155

---
+++ 155. Min Stack.js
@@ -0,0 +1,51 @@
+/**
+ * initialize your data structure here.
+ */
+
+// 法一：辅助栈
+// todo 法二：不用额外空间
+
+var MinStack = function() {
+    this.stack = []
+    this.supStack = [Infinity]
+};
+
+/** 
+ * @param {number} val
+ * @return {void}
+ */
+MinStack.prototype.push = function(val) {
+    this.stack.push(val)
+    this.supStack.push(Math.min(this.supStack[this.supStack.length - 1], val))
+};
+
+/**
+ * @return {void}
+ */
+MinStack.prototype.pop = function() {
+    this.stack.pop()
+    this.supStack.pop()
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.top = function() {
+    return this.stack[this.stack.length - 1]
+};
+
+/**
+ * @return {number}
+ */
+MinStack.prototype.getMin = function() {
+    return this.supStack[this.supStack.length - 1]
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * var obj = new MinStack()
+ * obj.push(val)
+ * obj.pop()
+ * var param_3 = obj.top()
+ * var param_4 = obj.getMin()
+ */
