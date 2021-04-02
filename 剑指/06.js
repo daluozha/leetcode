c3797b76fe317bce20040bf0cae1807aeee9343e
daluozha <561890199@qq.com>
2021-04-02 21:21:42 +0800

    jianzhi06

---
+++ 剑指/06.js
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {number[]}
+ */
+ var reversePrint = function(head) {
+    // 法一：正序输入数组，reverse输出
+    // let p = head, arr = []
+    // while(p){
+    //     arr.push(p.val)
+    //     p = p.next
+    // }
+    // return arr.reverse()
+
+    // 法二：反转链表再输出，详见lc 206
+
+    // 法三：递归
+    
+};
