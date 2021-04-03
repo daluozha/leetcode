7e1f7a406943334ec1dc8c93d5aceb5108cf646b
daluozha <561890199@qq.com>
2021-04-03 18:41:08 +0800

    240

--- 240. Search a 2D Matrix II.js
+++ 240. Search a 2D Matrix II.js
@@ -4,10 +4,23 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    let m=0,n=matrix[0].length-1
-    while(m<matrix.length && n>=0){
-        if(target==matrix[m][n]) return true
-        else target>matrix[m][n]?m++:n--
+    // 法一
+    // let row = matrix && matrix.length, column = matrix[0] && matrix[0].length
+    // if(!row || !column) return false
+    // for(let i = 0; i < row; i++){
+    //     for(let j = 0; j < column; j++){
+    //         if(matrix[i][j] === target) return true
+    //     }
+    // }
+    // return false
+
+    // 法二
+    if(!matrix  || !matrix[0]) return false
+    let row = matrix.length, 
+        i = 0, j = matrix[0].length - 1
+    while(i < row && j >= 0){
+        if(matrix[i][j] === target) return true
+        else matrix[i][j] > target ? j-- : i++
     }
     return false
 };
