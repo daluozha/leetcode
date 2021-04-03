0efe3b202e05d82794827e0255959b305e70c8e0
daluozha <561890199@qq.com>
2021-04-03 18:40:34 +0800

    lcof 04

---
+++ lcof/04.js
@@ -0,0 +1,26 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+ var findNumberIn2DArray = function(matrix, target) {
+    // 法一
+    // let row = matrix && matrix.length, column = matrix[0] && matrix[0].length
+    // if(!row || !column) return false
+    // for(let i = 0; i < row; i++){
+    //     for(let j = 0; j < column; j++){
+    //         if(matrix[i][j] === target) return true
+    //     }
+    // }
+    // return false
+
+    // 法二
+    if(!matrix  || !matrix[0]) return false
+    let row = matrix.length, 
+        i = 0, j = matrix[0].length - 1
+    while(i < row && j >= 0){
+        if(matrix[i][j] === target) return true
+        else matrix[i][j] > target ? j-- : i++
+    }
+    return false
+};
